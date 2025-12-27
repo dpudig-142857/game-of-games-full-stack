d6b545ce53493b33ab5afadfc0c34b77d50e5fb9
Pudwig23 <danpudig@outlook.com>
2025-12-27 17:59:08 +1100

    Custom domain for backend

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://game-of-games-backend.onrender.com';
-export const SESSION_URL = 'https://game-of-games-backend.onrender.com/api/sessions';
+export const BASE_URL = 'https://api.thegameofgames.win';
+export const SESSION_URL = 'https://api.thegameofgames.win/api/sessions';
