ab36f65301baceab8436f4004d8cc7513a31e34d
Pudwig23 <danpudig@outlook.com>
2025-12-29 22:35:18 +1100

    Print statement fix

--- js/user.js
+++ js/user.js
@@ -221,7 +221,7 @@ async function logout() {
 
 export async function loadUserOption() {
     user_data = await loadUser();
-    console.log(`user_data = ${user_data}`);
+    console.log(user_data);
     updateProfilePic(user_data);
     return user_data;
 }
