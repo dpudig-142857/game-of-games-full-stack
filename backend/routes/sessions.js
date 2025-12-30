41d3b24d12c9d2d0f0c2ac7d0f0fa3d471d8b1a0
Pudwig23 <danpudig@outlook.com>
2025-12-30 15:23:42 +1100

    Added clear routes

--- backend/routes/sessions.js
+++ backend/routes/sessions.js
@@ -2,11 +2,42 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('root'));
-router.get('/home', (req, res) => res.send('home'));
-router.get('/continue', (req, res) => res.send('continue'));
-router.get('/:id/creating', (req, res) => res.send('creating'));
-router.get('/:id/results', (req, res) => res.send('results'));
-router.get('/:id', (req, res) => res.send('catch-all'));
+router.get('/', async (req, res) => res.send('root'));
+
+router.get('/home', async (req, res) => res.send('home'));
+
+router.get('/incomplete', async (req, res) => res.send('incomplete'));
+
+router.get('/logs', async (req, res) => res.send('logs'));
+
+router.get('/continue', async (req, res) => res.send('continue'));
+
+router.get('/:id/creating', async (req, res) => res.send('id creating'));
+
+router.get('/:id/results', async (req, res) => res.send('id results'));
+
+router.post('/', async (req, res) => res.send('post /'));
+
+router.post('/:id/continue/active', async (req, res) => res.send('post id continue active'));
+
+router.post('/:id/continue/complete', async (req, res) => res.send('post id continue complete'));
+
+router.post('/:id/delete', async (req, res) => res.send('post id delete'));
+
+router.post('/:id/upload', async (req, res) => res.send('post id upload'));
+
+router.post('/:id/save', async (req, res) => res.send('post id save'));
+
+router.post('/:id/complete', async (req, res) => res.send('post id complete'));
+
+router.post('/:id/intrude', async (req, res) => res.send('post id intrude'));
+
+router.post('/:id/abandon', async (req, res) => res.send('post id abandon'));
+
+router.post('/:id/break_cone', async (req, res) => res.send('post id break_cone'));
+
+router.post('/:id/victory_cone', async (req, res) => res.send('post id victory_cone'));
+
+router.get('/:id', async (req, res) => res.send('id'));
 
 export default router;
